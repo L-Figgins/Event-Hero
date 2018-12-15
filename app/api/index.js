36a64e2a99fade4a4023d3e4e59c742fe70e0391
Logan Figgins <l.figgins13@gmail.com>
2018-12-14 17:40:09 -0800

    added client side api function to fetch albums from server side api

--- app/api/index.js
+++ app/api/index.js
@@ -10,7 +10,12 @@ function loadEventById(id) {
   return fetch(`/api/events/${id}`).then(response => response.json());
 }
 
+function loadAlbums() {
+  return fetch('/api/albums').then(response => response.json());
+}
+
 export const Api = {
   loadEvents,
   loadEventById,
+  loadAlbums,
 };
